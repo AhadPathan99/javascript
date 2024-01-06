d98db1fec5bb03eb6169fe4813bba2b88645e7ea
Ayan Khan <140692729+AhadPathan99@users.noreply.github.com>
2024-01-06 09:42:08 +0000

    Opration

--- besics-javascript/conversion.js
+++ besics-javascript/conversion.js
@@ -18,3 +18,25 @@ let someNomber = 22
 
 let stringNomer = String(someNomber)
 console.log(typeof someNomber);
+
+
+
+
+
+/******************** Opration ********************* */ 
+
+let str1 = "hello"
+let str2 = " Ayan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("2" + 1);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let gameCounter = 200
+++gameCounter;
+console.log(gameCounter);
