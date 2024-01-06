dd44b34073f4d58a43924777e80611a27bcd6751
Ayan Khan <140692729+AhadPathan99@users.noreply.github.com>
2024-01-06 08:44:29 +0000

    datatypes

---
+++ besics-javascript/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat use js code new version
+
+console.log( 3 + 3)
+
+console.log("Ayan")
+
+let name = ("Ayan")
+
+let age = 18
+
+let isLoggedIn = false
+
+console.log( typeof null); //undifined
+
+console.log(typeof undefined);  //object
---
+++ besics-javascript/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountEmail = "ak72@gmail.com"
+var accountPassword ="099"
+accountCity ="Delhi"
+let accountState;
+//accountId = 2  Not allowed
+accountEmail = "12wsdc@"
+accoountPassword ="222"
+accountCity ="noida"
+  let
+
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState])
