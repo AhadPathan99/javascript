c9a4ea26036d4418a53e1e7fbd4da4f1ac293897
Ayan Khan <140692729+AhadPathan99@users.noreply.github.com>
2024-01-06 09:13:30 +0000

    conversion datatypes

---
+++ besics-javascript/conversion.js
@@ -0,0 +1,20 @@
+let score = 33
+
+//console.log(typeof score);
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNomber = 22
+
+let stringNomer = String(someNomber)
+console.log(typeof someNomber);
